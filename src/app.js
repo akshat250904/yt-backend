9de1b9c1044f6c63ce0f14cfbb21423683144a75
akshat250904 <girdharakshat5@gmail.com>
2024-07-19 12:54:47 +0530

    util code discussion

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"    
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb"}))                               // json se kitna data aaega uski limit determine krne ke liye
+app.use(express.urlencoded({extended: true, limit: "16kb"}))         //to decode to original url
+app.use(express.static("public"))
+app.use(cookieParser())                                             //to get the access of users's cookie and uspe CRUD operation perfrom kar ske. 
+
+
+export {app}
