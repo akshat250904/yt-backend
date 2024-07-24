2e33ee4b54c5927ee9d53ec929e38d6264366aef
akshat250904 <girdharakshat5@gmail.com>
2024-07-24 14:54:46 +0530

    Added new routes for the controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
-import { loginUser,registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelprofile,
+    getUserWatchHistory,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -23,7 +34,13 @@ router.route("/login").post(loginUser)
 
 
 //secured routes
-router.route("/logout").post(verifyJWT , logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 router.routes("/refresh-token").post(refreshAccessToken)
-
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get( verifyJWT ,getCurrentUser)
+router.route("/account-details").patch(verifyJWT , updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+router.route("/update-coverimage").patch(verifyJWT, upload.single("coverimage"),updateUserCoverImage)
+router.route("/channel/:username").get(verifyJWT, getUserChannelprofile)     //params se data liya h isliye aise kra(/channel/:username) second slash ke bad ka data retrive hoga
+router.route("/watch-history").get(verifyJWT, getUserWatchHistory)
 export default router
