1686c479d274d2708e51e4c0dcd9659ae6626ea7
akshat250904 <girdharakshat5@gmail.com>
2024-07-31 15:02:01 +0530

    made the models, routes and controllers for various things

--- src/app.js
+++ src/app.js
@@ -1,6 +1,32 @@
+// import express from "express"
+// import cors from "cors"
+// import cookieParser from "cookie-parser"    
+
+// const app = express()
+
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+
+// app.use(express.json({ limit: "16kb"}))                               // json se kitna data aaega uski limit determine krne ke liye
+// app.use(express.urlencoded({extended: true, limit: "16kb"}))         //to decode to original url
+// app.use(express.static("public"))
+// app.use(cookieParser())                                             //to get the access of users's cookie and uspe CRUD operation perfrom kar ske. 
+
+
+// //import routes
+// import userRouter from './routes/user.routes.js'
+
+// //route declare
+// app.use("/api/v1/users", userRouter)
+
+
+// export {app}
+
 import express from "express"
 import cors from "cors"
-import cookieParser from "cookie-parser"    
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -9,17 +35,34 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({ limit: "16kb"}))                               // json se kitna data aaega uski limit determine krne ke liye
-app.use(express.urlencoded({extended: true, limit: "16kb"}))         //to decode to original url
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())                                             //to get the access of users's cookie and uspe CRUD operation perfrom kar ske. 
+app.use(cookieParser())
 
 
-//import routes
+//routes import
 import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
-//route declare
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
+// http://localhost:8000/api/v1/users/register
 
-export {app}
+export { app }
