bc753272d7ea151a7c4b710eb81b4ea505cc0e11
akshat250904 <girdharakshat5@gmail.com>
2024-08-05 17:21:44 +0530

    made some functions for comment and dashboard controllers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,6 +3,8 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js"
+import { Video } from "../models/video.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
@@ -13,14 +15,105 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    if(!req.user){
+        throw new ApiError(401,"User not logged in")
+    }
+
+    const videoId = req.params.videoId
+    const userId = req.user._id
+    const user = await User.findById(userId)
+    if(!user){
+        throw new ApiError(401,"User not found")
+    }
+
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(401,"Video not found")
+    }
+
+    const {content} = req.body
+
+    const newComment = new Comment({
+        content,
+        video: videoId,
+        owner: userId,
+    })  
+    
+    if(!newComment){
+        throw new ApiError(401,"Error while posting new comment")
+    }
+
+    await newComment.save()
+
+    return res.status(200)
+    .json(new ApiResponse(200, newComment, "Comment added successfully"))
+    
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    if(!req.user){
+        throw new ApiError(401,"user not logged in")
+    }
+
+    const userId = req.user._id
+    const commentId = req.params.commentId
+
+
+    const user = await User.findById(userId)
+    if(!user){
+        throw new ApiError(401,"User not found")
+    }
+    const {content} = req.body
+
+    if(!content){
+        throw new ApiError(401,"Content can not be empty")
+    }
+    const comment = await Comment.findByIdAndUpdate(
+        commentId,
+        {content: content},
+        { new: true}
+    )
+
+    if(!comment){
+        throw new ApiError(401,"Comment can't be posted")
+    }
+
+    await comment.save()
+
+    return res.status(200)
+    .json(new ApiResponse(200, comment, "Comment Updated Successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    if(!req.user){
+        throw new ApiError(401,"User not logged in")
+    }
+
+    const userId = req.user._id
+    const user = await User.findById(userId)
+    if(!user){
+        throw new ApiError(401,"User not found")
+    }
+
+    const commentId = req.params.commentId
+    
+
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(401,"Comment not found")
+    }
+    const ownerId = comment.owner
+
+    if(ownerId.toString() !== userId.toString()){
+        throw new ApiError(401,"You do not have access to delete the comment")
+    }
+    
+    await Comment.findByIdAndDelete(commentId)
+
+    return res.status(200)
+    .json(new ApiResponse(200, "comment deleted successfully"))
+
 })
 
 export {
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import { User } from "../models/user.model.js"
 import {Video} from "../models/video.model.js"
 import {Subscription} from "../models/subscription.model.js"
 import {Like} from "../models/like.model.js"
@@ -12,9 +13,28 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+
+    if(!req.user){
+        throw new ApiError(401,"User not logged in")
+    }
+
+    const userId = req.user._id
+    const user = await User.findById(userId)
+    if(!user){
+        throw new ApiError(401,"User not found")
+    }
+
+    const videos = await Video.find({ owner: userId.toString() });
+    if (videos.length === 0) {
+        throw new ApiError(401, "No videos found");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200,videos,"video retrieved successfully"))
+
 })
 
 export {
     getChannelStats, 
     getChannelVideos
-    }
+}
